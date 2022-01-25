25e1a191e3eca3a5cfa256dfe47897f0dc4cf3f2
Torben Berger <mail@torbenberger.de>
2022-01-25 22:45:32 +0100

    vans-jira-magic update

--- vans-jira-magic.user.js
+++ vans-jira-magic.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VANS Jira Magic
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Torben Berger
 // @match        https://jira.mercedes-benz.io/browse/VANS-*
